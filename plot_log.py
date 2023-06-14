6bfc4ebaa495f6fb2a5a0046778339914a0fd082
ddd <daiman4444@gmail.com>
2023-06-14 23:35:15 +0600

    csv was collected from the log file for further plotting

--- plot_log.py
+++ plot_log.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import csv
+import matplotlib as plt
+
+
 
 input_log = input('Указать файл лога:')
 output_log = input_log[::-1]
@@ -11,7 +15,7 @@ if index != -1:
 else:
     print("не удается определить путь для сохранения лога")
     
-output_log = output_log[::-1] + '/output.log'
+output_log = output_log[::-1] + '/output_log.csv'
 backup_log = input_log + '.bak'
 
 with open(input_log, 'r') as file:
@@ -30,7 +34,6 @@ log_lines_output = []
 headers_line = log_lines[0]
 
 lines_num =  len(log_lines)
-lines_counter = 0
 
 print(lines_num)
 
@@ -39,19 +42,43 @@ headers = headers[0::2]
 
 print(headers)
 
-while lines_counter <= lines_num:
-    
-
-
-
-for line in log_lines:
+for i in range(lines_num):
     for header in headers:
-        header = str(header)
-        new_line = line.replace(header, "")
-    #log_lines_output.append(new_line)
+        log_lines[i] = log_lines[i].replace(header, '')
+        log_lines[i] = log_lines[i].replace("+", '')
+        log_lines[i] = log_lines[i].replace(",", '.')
+        
+#TODO разбить строки на элементы списка
 
-with open(output_log, 'w') as file:
-    file.writelines(log_lines)
+with open(output_log, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows([s] for s in log_lines)
+    
+y1_values = []
+y2_values = []
+y3_values = []
+y4_values = []    
     
     
-    
+with open(output_log, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        y1_values.append(float(row[0]))
+        y2_values.append(float(row[1]))
+        y3_values.append(float(row[2]))
+        y4_values.append(float(row[3]))
+        
+x_values = range(len(y1_values))
+
+plt.plot(x_values, y1_values, label="Y1")
+plt.plot(x_values, y2_values, label="Y2")
+plt.plot(x_values, y3_values, label="Y3")
+plt.plot(x_values, y4_values, label="Y4")
+
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Графики из CSV")
+plt.grid(True)
+plt.legend()
+plt.show()
+
